c3ac2ed175f69fb458e409d6eede915cdab59f11
roywaswa <roywaswa.rw@gmail.com>
2024-08-19 16:42:37 +0300

    feat: Caching episodes request from buzzsprout

--- src/app/api/buzzsprout/route.js
+++ src/app/api/buzzsprout/route.js
@@ -6,7 +6,7 @@ import { NextResponse } from "next/server";
 const BASE_URL = process.env.BUZZSPROUT_BASE_URL
 const PODCAST_ID = process.env.BUZZSPROUT_PODCAST_IDENTIFIER
 const API_TOKEN = process.env.BUZZSPROUT_API_TOKEN
-
+const seconds_in_day = 60*60*24
 
 export async function GET() {
   const url = `${BASE_URL}/${PODCAST_ID}/episodes.json`
@@ -17,7 +17,8 @@ export async function GET() {
       "Content-Type": "application/json",
       "charset":"utf-8",
       "Authorization": `Token token=${API_TOKEN}`
-    }
+    },
+    next: { revalidate: seconds_in_day/5 }
   })
 
   let data  = await response.json()
