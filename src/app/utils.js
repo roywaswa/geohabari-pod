8d0ae05f6636bbb9c98528f4377b56127d7d67f0
roywaswa <roywaswa.rw@gmail.com>
2024-11-02 07:26:03 +0300

    feat: first hero done

--- src/app/utils.js
+++ src/app/utils.js
@@ -25,3 +25,37 @@ export const platform_links = {
     "applepods":"https://podcasts.apple.com/ke/podcast/geohabari/id1747885525",
     "podcast_addict": "https://podcastaddict.com/podcast/geohabari/5021923" 
 }
+
+
+export const topics = [
+  {
+    title: "Tech",
+    subtitle: "Highlighting Africa's Tech Innovators",
+    description: "Discover African organizations leading in geo-enabling and other cutting-edge technologies. We dive into the systems' components, from programming languages to seamless integrations, showcasing impactful use cases across the continent."
+  },
+  {
+    title: "Geo-Banter",
+    subtitle: "Your Ultimate Tech Therapy Sessions",
+    description: "Blend technical expertise with humor to humanize experiences in the tech world. Ideal for those looking for a light-hearted, tech-centric laugh."
+  },
+  {
+    title: "Earth",
+    subtitle: "Exploring EO Applications",
+    description: "Dive into Earth Observation (EO) use cases across various sectors. From technical tools and datasets to real-world applications and impacts, each episode offers a comprehensive look at EO’s potential."
+  },
+  {
+    title: "Action",
+    subtitle: "Tech Making a Difference",
+    description: "Explore how tech is used across Africa to drive change. Highlighting limitless possibilities, we showcase how GIS is leveraged for impactful solutions in different industries."
+  },
+  {
+    title: "Career",
+    subtitle: "Geospatial Career Journeys",
+    description: "Navigate diverse career paths in the geospatial industry. Discussing pivotal moments, key learnings, and challenges, these episodes inspire you to explore new career horizons."
+  },
+  {
+    title: "Scholarships",
+    subtitle: "Your Study Abroad Guide",
+    description: "Scholarship recipients share their application tips and tricks, covering everything from requirements to living abroad. The ultimate starter pack for geospatial students looking to study overseas."
+  }
+];
