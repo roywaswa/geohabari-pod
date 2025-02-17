2bddf4bc60214684aff6ace6c7b6a92de4337d37
Roy Waswa <roywaswa.rw@gmail.com>
2025-02-17 12:29:52 +0300

    feat: enhance dark mode switch with icons and improved styling

--- src/components/DarkModeSwitch/DarkModeSwitch.jsx
+++ src/components/DarkModeSwitch/DarkModeSwitch.jsx
@@ -2,14 +2,22 @@
 import React from 'react';
 import styles from './DarkModeSwitch.module.scss';
 import { useTheme } from '@/context/ThemeContext';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 
 export default function DarkModeSwitch() {
-  const { isDarkMode,toggleTheme } = useTheme()
+  const { isDarkMode, toggleTheme } = useTheme();
   return (
-    <div onClick={toggleTheme} className={styles.dark_mode_switch}>
-      {
-        isDarkMode ? <span>Light Mode</span> :<span>Dark Mode</span>
-      }
+    <div className={styles.switch_container}>
+      <div onClick={toggleTheme} className={`${styles.dark_mode_switch} ${isDarkMode ? styles.dark : styles.light}`}>
+        <div className={styles.icon}>
+          {
+            isDarkMode ? 
+            <FontAwesomeIcon icon={faSun} size="lg" /> : 
+            <FontAwesomeIcon icon={faMoon} size="lg" />
+          }
+        </div>
+      </div>
     </div>
-  )
+  );
 }
