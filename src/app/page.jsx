1f19e7e4c639958d6483b9bf2598d97317079de4
Roy Waswa <roywaswa.rw@gmail.com>
2025-06-05 09:54:29 +0300

    feat: implement NewsLetter component with form and styling; refactor page layout

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.scss";
-import { TopicsSection,AboutHost,NewsLetter,LatestEpisodes } from "./pageComponents";
+// import { TopicsSection,AboutHost,NewsLetter,LatestEpisodes } from "./pageComponents";
 import HeroSection from "@/sections/HeroSection/HeroSection.lazy";
 import EpisodeBanner from "@/components/EpisodeBanner/EpisodeBanner";
 import LatestEp from "@/sections/LatestEp/LatestEp";
 import ShowThemes from "@/sections/ShowThemes/ShowThemes";
 import RecentEps from "@/sections/RecentEps/RecentEps";
 import QuoteCards from "@/sections/QuoteCards/QuoteCards";
+import NewsLetter from "@/sections/NewsLetter/NewsLetter";
 
 
 export default function Home() {
@@ -17,6 +18,7 @@ export default function Home() {
       <ShowThemes />
       <RecentEps/>
       <QuoteCards />
+      <NewsLetter />
     </main>
   );
 }
---
+++ src/sections/NewsLetter/NewsLetter.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './NewsLetter.module.scss';
+
+export default function NewsLetter() {
+
+  return (
+    <div className={styles.NewsLetter}>
+      <div className={styles.form_copy}>
+        <h2>JOIN THE COMMUNITY</h2>
+        <p>Welcome to the community, stay upto speed with our latest and greatest ... </p>
+      </div>
+      <form className={styles.newsletter_form}>
+        <input
+          type="text"
+          placeholder="Enter your name"
+          className={styles.form_input}
+          required
+        />  
+        <input
+          type="email"
+          placeholder="Enter your email address"
+          className={styles.form_input}
+          required
+        />
+        <button type="submit" className={styles.form_button}>
+          Subscribe
+        </button>
+      </form>
+    </div>
+  )
+}
